20845851ee3eb75dca34397129488a7362a88f1e
Patrick Leber <patrick.leber@freenet.de>
2016-07-31 18:43:19 +0200

    Initial commit

---
+++ core/src/main/scala/control/MainControl.scala
@@ -0,0 +1,9 @@
+package main.scala.control
+
+import main.scala.engine.Point
+
+
+object MainControl extends App {
+  val p = Point(1, 2)
+  println(p)
+}
---
+++ core/src/main/scala/engine/Point.scala
@@ -0,0 +1,3 @@
+package main.scala.engine
+
+case class Point(x: Double, y: Double)
