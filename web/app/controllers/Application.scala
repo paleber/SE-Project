795ccbfc16634ae073acdbd9137fb8e0e7e19aa2
Patrick Leber <patrick.leber@freenet.de>
2017-01-09 18:06:34 +0100

    add comet

--- web/app/controllers/Application.scala
+++ web/app/controllers/Application.scala
@@ -2,16 +2,22 @@ package controllers
 
 import javax.inject.Inject
 
-import akka.actor.ActorSystem
-import akka.stream.Materializer
+import akka.actor.{ActorRef, ActorSystem}
+import akka.stream.{Materializer, OverflowStrategy}
+import akka.stream.scaladsl.{Sink, Source}
 import akka.util.Timeout
 import control.MainControl
 import gui.Gui
 import models.Wui
+import play.api.http.ContentTypes
+import play.api.libs.Comet
+import play.api.libs.json.{JsString, JsValue}
 import play.api.libs.streams.ActorFlow
 import play.api.mvc._
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
+
 
 class Application @Inject()(implicit system: ActorSystem, mat: Materializer) extends Controller {
 
@@ -54,5 +60,24 @@ class Application @Inject()(implicit system: ActorSystem, mat: Materializer) ext
     system.actorOf(Gui.props(control))
     ActorFlow.actorRef(socket => Wui.props(control, socket))
   }
+
+
+
+  def cometJson = Action {
+    implicit val m = mat // TODO needed here again?
+    //def jsonSource: Source[JsValue, _] = Source(List(JsString("jsonString")))
+
+    val s = Source
+      .actorRef[String](bufferSize = 1000, OverflowStrategy.dropHead)
+      .mapMaterializedValue(actor => {
+        Future { Thread.sleep(300); actor ! 1 }
+        Future { Thread.sleep(200); actor ! 2 }
+        Future { Thread.sleep(100); actor ! 3 }
+      })
+
+    Ok.chunked(s via Comet.string("parent.cometMessage")).as(ContentTypes.HTML)
+  }
+
+
 
 }
