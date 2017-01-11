68a99e965f7e4d3065dbab7b338957a97ecfc45c
Patrick Leber <paleber@htwg-konstanz.de>
2017-01-11 22:07:07 +0100

    cleaning code

--- web/app/assets/app/app.module.ts
+++ web/app/assets/app/app.module.ts
@@ -1,15 +1,18 @@
-import "./rxjs-extensions";
-import {NgModule} from "@angular/core";
-import {BrowserModule} from "@angular/platform-browser";
-import {FormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
-import {AppRoutingModule} from "./app-routing.module";
-import {AppComponent} from "./app.component";
-import {DashboardComponent} from "./dashboard.component";
-import {HeroesComponent} from "./heroes.component";
-import {HeroDetailComponent} from "./hero-detail.component";
-import {HeroService} from "./hero.service";
-import {HeroSearchComponent} from "./hero-search.component";
+import './rxjs-extensions';
+
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }          from './hero.service';
+import { HeroSearchComponent }  from './hero-search.component';
 
 @NgModule({
     imports: [
