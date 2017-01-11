651ee809fe396d9d62a6153c0829b37ffd72f88f
Patrick Leber <paleber@htwg-konstanz.de>
2017-01-12 00:32:39 +0100

    add angular login

--- web/app/assets/app/app.module.ts
+++ web/app/assets/app/app.module.ts
@@ -1,18 +1,19 @@
 import './rxjs-extensions';
 
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { HttpModule }    from '@angular/http';
+import {NgModule}      from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule}   from '@angular/forms';
+import {HttpModule}    from '@angular/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 
-import { AppComponent }         from './app.component';
-import { DashboardComponent }   from './dashboard.component';
-import { HeroesComponent }      from './heroes.component';
-import { HeroDetailComponent }  from './hero-detail.component';
-import { HeroService }          from './hero.service';
-import { HeroSearchComponent }  from './hero-search.component';
+import {AppComponent}         from './app.component';
+import {DashboardComponent}   from './dashboard.component';
+import {HeroesComponent}      from './heroes.component';
+import {HeroDetailComponent}  from './hero-detail.component';
+import {HeroService}          from './hero.service';
+import {HeroSearchComponent}  from './hero-search.component';
+import {LoginComponent}       from './login.component';
 
 @NgModule({
     imports: [
@@ -27,11 +28,13 @@ import { HeroSearchComponent }  from './hero-search.component';
         HeroDetailComponent,
         HeroesComponent,
         HeroSearchComponent,
+        LoginComponent,
     ],
-    providers: [ HeroService ],
-    bootstrap: [ AppComponent ],
+    providers: [HeroService],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
 
 /*
  Copyright 2016 Google Inc. All Rights Reserved.
