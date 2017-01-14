a3947d0d19de46afb1ef4fc0f58ed6d01290a1a4
Patrick Leber <patrick.leber@freenet.de>
2017-01-14 14:30:04 +0100

    angular integration

--- web/app/controllers/Application.scala
+++ web/app/controllers/Application.scala
@@ -90,13 +90,8 @@ class Application @Inject()(silhouette: Silhouette[DefaultEnv],
     Ok.chunked(source via Comet.string("parent.cometPush")).as(ContentTypes.HTML)
   }
 
-  def angular = Action {
-    /** change the template here to use a different way of compilation and loading of the ts ng2 app.
-      * index()  :    does no ts compilation in advance. the ts files are download by the browser and compiled there to js.
-      * index1() :    compiles the ts files to individual js files. Systemjs loads the individual files.
-      * index2() :    add the option -DtsCompileMode=stage to your sbt task . F.i. 'sbt ~run -DtsCompileMode=stage' this will produce the app as one single js file.
-      */
-    Ok(views.html.index1())
+  def angular = silhouette.UserAwareAction { request =>
+    Ok(views.html.angular(request.identity))
   }
 
   //def notFound(notFound: String) = Default.notFound
