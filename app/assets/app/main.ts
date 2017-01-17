cdfe0f7608e0bf8c57960983599d6478e6da230d
Patrick Leber <paleber@htwg-konstanz.de>
2017-01-17 21:57:06 +0100

    bug fixing

--- app/assets/app/main.ts
+++ app/assets/app/main.ts
@@ -1,4 +1,6 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule }              from './app.module';
+import {enableProdMode}         from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {AppModule}              from './app.module';
 
+enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
