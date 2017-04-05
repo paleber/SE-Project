39da70b8644bd7edcc365f9f98b4e06014bdd99a
Patrick Leber <paleber@htwg-konstanz.de>
2017-04-05 19:01:50 +0200

    implement db4o

--- core/src/main/scala/persistence/Db4oPersistence.scala
+++ core/src/main/scala/persistence/Db4oPersistence.scala
@@ -6,27 +6,35 @@ import org.json4s.NoTypeHints
 import org.json4s.jackson.Serialization
 import org.json4s.jackson.Serialization.{read, write}
 
-import scala.util.Try
-
+import scala.collection.JavaConversions._
 
 class Db4oPersistence extends Persistence {
 
-  private case class Db4oEntry(category: String, name: String, plan: String)
-
   private implicit val formats = Serialization.formats(NoTypeHints)
 
-  private def openDatabase(): ObjectContainer = {
-    Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "db4o-database")
+  private case class Db4oEntry(category: String, name: String, plan: String)
+
+  def doDatabaseAction[T](f: ObjectContainer => T): T = {
+    val db = Db4oEmbedded.openFile(
+      Db4oEmbedded.newConfiguration(),
+      "db4o-database"
+    )
+    try {
+      f(db)
+    } finally {
+      db.close()
+    }
   }
 
-  override def loadMetaInfo: Map[String, List[String]] = {
-    // TODO
-    null
+  override def loadMetaInfo: Map[String, List[String]] = doDatabaseAction { db =>
+    val query = db.query()
+    query.constrain(classOf[Db4oEntry])
+    val set = query.execute[Db4oEntry]()
+    val categories: List[String] = set.map(_.category).toList
+    categories.map(category => (category, set.filter(_.category == category).map(_.name).toList)).toMap
   }
 
-  override def loadPlan(levelId: LevelId): Plan = {
-
-    val db = openDatabase()
+  override def loadPlan(levelId: LevelId): Plan = doDatabaseAction { db =>
     val query = db.query()
     query.constrain(classOf[Db4oEntry])
     query.descend("category").constrain(levelId.category).equal()
@@ -34,61 +42,22 @@ class Db4oPersistence extends Persistence {
 
     val set = query.execute[Db4oEntry]()
     if (set.size != 1) {
-      db.close()
       throw new NoSuchElementException("plan not found")
     }
-
-    val plan = read[Plan](set.get(0).plan)
-    db.close()
-    plan
-
+    read[Plan](set.get(0).plan)
   }
 
-  override def savePlan(levelId: LevelId, plan: Plan): Unit = {
-    if (Try(loadPlan(levelId)).isSuccess) {
+  override def savePlan(levelId: LevelId, plan: Plan): Unit = doDatabaseAction { db =>
+    val query = db.query()
+    query.constrain(classOf[Db4oEntry])
+    query.descend("category").constrain(levelId.category).equal()
+    query.descend("name").constrain(levelId.name).equal()
+
+    val set = query.execute[Db4oEntry]()
+    if (set.size > 0) {
       throw new IllegalArgumentException("LevelId already exists")
     }
-
-    val db = openDatabase()
     db.store(Db4oEntry(levelId.category, levelId.name, write(plan)))
-    db.close()
   }
 
-
-
-
-
 }
-
-
-object x extends App {
-
-  val p = new Db4oPersistence
-
-
-  val y = p.loadPlan(LevelId("abc", "lv1"))
-  println(y)
-
-}
-
-object a extends App {
-
-  val p = new Db4oPersistence
-
-  val plan = Plan(4, List(
-
-
-    List(List(3, 0), List(3)),
-    List(List(2), List(2, 1))
-  ))
-
-  println(plan)
-
-  p.savePlan(
-    LevelId("abc", "lv1"),
-    plan
-  )
-
-
-}
-
